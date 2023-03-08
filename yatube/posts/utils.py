fba7e65753a214b94409bc9eb1fb2e3a5468c6c9
Olvenn <olga-vencel@yandex.ru>
2023-03-08 15:18:27 +0300

    Fix comments on writing code.

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -4,5 +4,4 @@ from django.core.paginator import Paginator
 def create_page_object(request, posts, count):
     paginator = Paginator(posts, count)
     page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return page_obj
+    return paginator.get_page(page_number)
