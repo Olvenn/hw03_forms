f5cec183e70d5c48b35a2b54eb2bba84e4778f3b
Olvenn <olga-vencel@yandex.ru>
2023-03-11 17:20:11 +0300

    Test whether the value of the __str__ field is displayed correctly in model objects.

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -1,9 +1,34 @@
-# Каждый логический набор тестов — это класс,
-# который наследуется от базового класса TestCase
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from ..models import Group, Post
 
-class Test(TestCase):
-    def test_example(self):
-        # пишем тест тут
-        pass
+User = get_user_model()
+
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='auth')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='Тестовый слаг',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый пост',
+        )
+
+    def test_verbose_name(self):
+        """verbose_name в полях совпадает с ожидаемым."""
+        test_items = {
+            PostModelTest.group: PostModelTest.group.title,
+            PostModelTest.post: PostModelTest.post.text[:15],
+        }
+
+        for field, expected_value in test_items.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    str(field), expected_value)
