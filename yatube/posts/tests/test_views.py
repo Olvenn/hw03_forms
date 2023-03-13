fa164214c78a564a073ee5631c1cfb01771fbe4e
Olvenn <olga-vencel@yandex.ru>
2023-03-13 21:51:50 +0300

    Create correct context tests.

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
+from django import forms
+
 
 from ..models import Group, Post
 
@@ -12,7 +14,6 @@ class PostViewTests(TestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.guest_client = Client()
-        # Создадим запись в БД для проверки доступности адреса task/test-slug/
         cls.user = User.objects.create_user(username='TestUser')
         cls.authorized_client = Client()
         cls.authorized_client.force_login(cls.user)
@@ -23,20 +24,23 @@ class PostViewTests(TestCase):
             slug='test-slug',
         )
 
+        cls.author = User.objects.create_user(
+            first_name='Лев',
+            last_name='Толстой',
+            username='FirstAuthor',
+            email='lev@yatube.ru'
+        )
+
         cls.post = Post.objects.create(
-            author=cls.user,
+            author=cls.author,
             group=cls.group,
             text='Тестовый пост',
         )
 
     def setUp(self):
-        # Создаем неавторизованный клиент
         self.guest_client = Client()
-        # Создаем пользователя
         self.user = User.objects.create_user(username='HasNoName')
-        # Создаем второй клиент
         self.authorized_client = Client()
-        # Авторизуем пользователя
         self.authorized_client.force_login(self.user)
 
     def test_page_has_correct_template(self):
@@ -52,8 +56,6 @@ class PostViewTests(TestCase):
             reverse('posts:post_create'): 'posts/create_post.html',
             reverse('posts:post_detail', kwargs={'post_id':
                     f'{self.post.id}'}): 'posts/post_detail.html',
-            reverse('posts:post_edit', kwargs={'post_id':
-                    f'{self.post.id}'}): 'posts/post_create.html'
         }
         # Проверяем, что при обращении
         # к name вызывается соответствующий HTML-шаблон
@@ -66,3 +68,49 @@ class PostViewTests(TestCase):
     #     """URL-адрес использует шаблон deals/home.html."""
     #     response = self.authorized_client.get(reverse('posts:index'))
     #     self.assertTemplateUsed(response, 'posts/index.html')
+
+    def test_create_post_page_show_correct_context(self):
+        """Форма добавления поста сформирована с правильным контекстом"""
+
+        response = self.authorized_client.get(reverse('posts:post_create'))
+        form_fields = {
+            'group': forms.fields.ChoiceField,
+            'text': forms.fields.CharField,
+        }
+
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_fields = response.context['form'].fields[value]
+                self.assertIsInstance(form_fields, expected)
+
+    def test_home_page_show_correct_context(self):
+        """Пост отображается на главной странице"""
+        response = self.authorized_client.get(reverse('posts:index'))
+        first_object = response.context['page_obj'][0]
+
+        post_text_0 = first_object.text
+        post_group_0 = first_object.group.title
+        post_author_0 = first_object.author.username
+        self.assertEqual(post_text_0, 'Тестовый пост')
+        self.assertEqual(post_group_0, 'Тестовая группа')
+        self.assertEqual(post_author_0, 'FirstAuthor')
+
+    def test_home_page_show_correct_context(self):
+        """Пост отображается на главной странице"""
+        response = self.authorized_client.get(reverse('posts:index'))
+        first_object = response.context['page_obj'][0]
+
+        post_text_0 = first_object.text
+        post_group_0 = first_object.group.title
+        post_author_0 = first_object.author.username
+        self.assertEqual(post_text_0, 'Тестовый пост')
+        self.assertEqual(post_group_0, 'Тестовая группа')
+        self.assertEqual(post_author_0, 'FirstAuthor')
+
+    def test_post_detail_pages_show_correct_context(self):
+        """Шаблон post_detail сформирован с правильным контекстом."""
+        response = (self.authorized_client.get(reverse('posts:post_detail',
+                    kwargs={'post_id': f'{self.post.id}'})))
+        self.assertEqual(response.context.get('post').group.title,
+                         'Тестовая группа')
+        self.assertEqual(response.context.get('post').text, 'Тестовый пост')
