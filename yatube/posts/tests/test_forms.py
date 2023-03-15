6f91f2f14b30e47a668d8e7b99a06d6e0620b3a0
Olvenn <olga-vencel@yandex.ru>
2023-03-15 19:15:21 +0300

    Create torms tests.

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,9 +1,73 @@
-# Каждый логический набор тестов — это класс,
-# который наследуется от базового класса TestCase
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from posts.models import Group, Post
+
+User = get_user_model()
 
 
-class Test(TestCase):
-    def test_example(self):
-        # пишем тест тут
-        pass
+class PostFormTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.author = User.objects.create(username='NewTestUser')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='test-slug',
+            description='Тестовое описание'
+        )
+        cls.post = Post.objects.create(
+            author=cls.author,
+            text='Тестовый пост',
+            group=cls.group
+        )
+
+    def setUp(self):
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.author)
+
+    def test_post_form_create_new_post(self):
+        posts_count = Post.objects.count()
+        form_data = {
+            'group': self.group.id,
+            'text': 'Тестовый текст из формы',
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True
+        )
+        self.assertRedirects(response, reverse('posts:profile',
+            kwargs={'username': self.post.author}))
+        self.assertEqual(Post.objects.count(), posts_count + 1)
+        self.assertTrue(Post.objects.filter(
+            text='Тестовый текст из формы',
+            group=self.group.id
+        ).exists())
+
+    def test_post_edit_correct(self):
+        posts_count = Post.objects.count()
+        form_data = {
+            'text': 'Измененный текст из формы',
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_edit', kwargs={
+                'post_id': self.post.id
+            }),
+            data=form_data,
+            follow=True,
+        )
+        # Проверяем, сработал ли редирект
+        self.assertRedirects(response, reverse('posts:post_detail', kwargs={
+            'post_id': self.post.id
+        }))
+        # Проверяем, что кол-во постов не изменилось
+        self.assertEqual(Post.objects.count(), posts_count)
+        # Проверяем, что у поста изменился текст
+        self.assertTrue(
+            Post.objects.filter(
+                id=self.post.id,
+                text=form_data['text'],
+            ).exists()
+        )
